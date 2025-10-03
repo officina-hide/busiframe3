e73afcc18132379831fd0d245359866ccc32df13
KACOMSAD\ueno <hideo@kacoms.co.jp>
2025-10-03 16:50:42 +0900

    環境情報クラスにデータベース接続関連の項目とgetter/setterを実装

--- src/main/java/busiframe3/core/Environmwnt.java
+++ src/main/java/busiframe3/core/Environmwnt.java
@@ -1,10 +1,37 @@
 package busiframe3.core;
 
 /**
- * 環境情報クラス<br>
+ * 環境クラス<br>
+ * このクラスは、プロジェクトの環境設定を管理するためのクラスです。<br>
+ * This class is responsible for managing the environment settings of the project.<br>
  * @since 2025/09/30
  * @version 1.0 2025/09/30 新規作成
  */
 public class Environmwnt {
+	
+	/** DB : データソース名 */
+	private String dsn;
+	/** DB : ユーザー名 */
+	private String user;
+	/** DB : パスワード */
+	private String password;
+	public String getDsn() {
+		return dsn;
+	}
+	public void setDsn(String dsn) {
+		this.dsn = dsn;
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 }
